a8b2513aad3a6c33e4384e2e661b16dfc0561163
raja <sainiraja59@gmail.com>
2019-10-29 14:23:56 +0530

    firebase added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,31 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
+import { environment } from '../environments/environment.prod';
 import { FormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
 import { MessageComponent } from './message/message.component';
+import { UploadComponent } from './upload/upload.component';
 
+
+// import { AngularFireModule } from "@angular/fire";
+// import { AngularFireStorage } from "@angular/fire/storage";
+
+import { AngularFireModule } from "angularfire2";
+import { AngularFireStorageModule } from "angularfire2/storage";
 
 @NgModule({
   declarations: [
     AppComponent,
-    MessageComponent
+    MessageComponent,
+    UploadComponent
   ],
   imports: [
     BrowserModule,
+   
     AppRoutingModule,HttpClientModule,FormsModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    AngularFireModule.initializeApp(environment.firebase),AngularFireStorageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyA7-NMNxMLNzZs-IuRXhDhSwCy9t226kVA",
+    authDomain: "mapa-96be5.firebaseapp.com",
+    databaseURL: "https://mapa-96be5.firebaseio.com",
+    projectId: "mapa-96be5",
+    storageBucket: "mapa-96be5.appspot.com",
+    messagingSenderId: "836706473960",
+    appId: "1:836706473960:web:a9b5c9b69bb521b798b65d",
+    measurementId: "G-VRCE3SSYCQ"
+
+  }
 };
